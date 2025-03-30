89c16d023c90e9e66f189be0a4881ecc9d325e26
Eduardo Magariño <145311850+Eduardo-04@users.noreply.github.com>
2025-03-29 19:13:48 -0600

    Add files via upload
    Usando mongodb

---
+++ productos/server.js
@@ -0,0 +1,11 @@
+﻿const express = require('express');
+const mongoose = require('mongoose');
+const productRoutes = require('./src/routes/routes');
+
+const app = express();
+mongoose.connect('mongodb://localhost:27017/productos_db');
+
+app.use(express.json());
+app.use('/api/products', productRoutes);
+
+app.listen(3000, () => console.log('Servidor en http://localhost:3000'));
